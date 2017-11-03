7f5952da727d64fba69a4f10cf834720984af70b
Andrew <heldiggris7@gmail.com>
2017-11-03 11:06:59 -0500

    update Greetings

---
+++ Greetings/Greetings/Form1.Designer.vb
@@ -0,0 +1,98 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnShowGreeting = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.txtUserName = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblGreetings = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnShowGreeting
+        '
+        Me.btnShowGreeting.Location = New System.Drawing.Point(70, 169)
+        Me.btnShowGreeting.Name = "btnShowGreeting"
+        Me.btnShowGreeting.Size = New System.Drawing.Size(77, 34)
+        Me.btnShowGreeting.TabIndex = 0
+        Me.btnShowGreeting.Text = "Show Greeting"
+        Me.btnShowGreeting.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(227, 169)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(77, 34)
+        Me.btnExit.TabIndex = 1
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'txtUserName
+        '
+        Me.txtUserName.Location = New System.Drawing.Point(96, 59)
+        Me.txtUserName.Name = "txtUserName"
+        Me.txtUserName.Size = New System.Drawing.Size(206, 20)
+        Me.txtUserName.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Comic Sans MS", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(128, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(136, 23)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Enter Your Name"
+        '
+        'lblGreetings
+        '
+        Me.lblGreetings.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblGreetings.Location = New System.Drawing.Point(99, 92)
+        Me.lblGreetings.Name = "lblGreetings"
+        Me.lblGreetings.Size = New System.Drawing.Size(200, 32)
+        Me.lblGreetings.TabIndex = 4
+        Me.lblGreetings.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(433, 248)
+        Me.Controls.Add(Me.lblGreetings)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtUserName)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnShowGreeting)
+        Me.Name = "Form1"
+        Me.Text = "Greetings"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnShowGreeting As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents txtUserName As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents lblGreetings As System.Windows.Forms.Label
+
+End Class
