d0b7dfc0f74062a3ca93fac9cf4a9d37da7d539b
Andrew <heldiggris7@gmail.com>
2017-11-14 10:22:06 -0600

    update TestScoreAverage

--- TestScoreAverage/TestScoreAverage/Form1.vb
+++ TestScoreAverage/TestScoreAverage/Form1.vb
@@ -1,14 +1,29 @@
 ﻿Public Class Form1
 
     Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
-        Dim decAverageScore As Decimal
+        Dim decAverageScore, dectotal As Decimal
 
         Try
-            decAverageScore = (txtScore1.Text) + (txtScore2.Text) + (txtScore3.Text) + (txtScore4.Text) + (txtScore5.Text)
-            lblAverage.Text = decAverageScore.ToString
+            dectotal = CDec(txtScore1.Text) + CDec(txtScore2.Text) + CDec(txtScore3.Text) + CDec(txtScore4.Text) + CDec(txtScore5.Text)
+            decAverageScore = dectotal / 5
+            lblAverage.Text = decAverageScore.ToString("n2")
         Catch
-
+            lblError.Text = "Enter numaric values into all input."
         End Try
 
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtScore1.Clear()
+        txtScore2.Clear()
+        txtScore3.Clear()
+        txtScore4.Clear()
+        txtScore5.Clear()
+        lblAverage.Text = String.Empty
+        lblError.Text = String.Empty
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
     End Sub
 End Class
